4150b1997848c4497567a55d4d74aa9a4672aee3
DA-RAE <styd4957@gmail.com>
2026-01-21 10:16:07 +0900

    fix: 심각한 이슈 수정 - @EnableScheduling 추가 (FrameBufferService 타임아웃 동작) - WebhookController에 알림 생성 로직 추가

--- src/main/java/com/aegis/aegisbackend/global/config/AsyncConfig.java
+++ src/main/java/com/aegis/aegisbackend/global/config/AsyncConfig.java
@@ -2,9 +2,16 @@ package com.aegis.aegisbackend.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * 비동기 및 스케줄링 설정
+ * - @EnableAsync: 비동기 메서드 실행 지원
+ * - @EnableScheduling: @Scheduled 어노테이션 지원 (FrameBufferService 타임아웃 등)
+ */
 @Configuration
 @EnableAsync
+@EnableScheduling
 public class AsyncConfig {
 }
 
